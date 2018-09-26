239fcdcd6658af44df199f115b4d74bae1a2c421
joey <joey.su@huolala.cn>
2018-09-26 13:45:08 +0800

    修改title

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Keep Moving' });
+  res.render('index', { title: '时间的朋友' });
 });
 
 module.exports = router;
--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -4,7 +4,7 @@
       <side-menu accordion ref="sideMenu" :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
         <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
         <div class="logo-con">
-          Keep Moving
+          时间的朋友
           <!-- <img v-show="!collapsed" :src="maxLogo" key="max-logo" /> -->
           <!-- <img v-show="collapsed" :src="minLogo" key="min-logo" /> -->
         </div>
