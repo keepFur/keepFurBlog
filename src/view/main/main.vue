d704e81a4fc3104efd6e05cd0b2cc74c691d1b26
joey <joey.su@huolala.cn>
2018-09-26 13:41:22 +0800

    修改title

--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -34,19 +34,19 @@
   </Layout>
 </template>
 <script>
-import SideMenu from './components/side-menu'
-import HeaderBar from './components/header-bar'
-import TagsNav from './components/tags-nav'
-import User from './components/user'
-import Fullscreen from './components/fullscreen'
-import Language from './components/language'
-import { mapMutations, mapActions } from 'vuex'
-import { getNewTagList, getNextRoute, routeEqual } from '@/libs/util'
-import minLogo from '@/assets/images/logo-min.jpg'
-import maxLogo from '@/assets/images/logo.jpg'
-import './main.less'
+import SideMenu from "./components/side-menu";
+import HeaderBar from "./components/header-bar";
+import TagsNav from "./components/tags-nav";
+import User from "./components/user";
+import Fullscreen from "./components/fullscreen";
+import Language from "./components/language";
+import { mapMutations, mapActions } from "vuex";
+import { getNewTagList, getNextRoute, routeEqual } from "@/libs/util";
+import minLogo from "@/assets/images/logo-min.jpg";
+import maxLogo from "@/assets/images/logo.jpg";
+import "./main.less";
 export default {
-  name: 'Main',
+  name: "Main",
   components: {
     SideMenu,
     HeaderBar,
@@ -55,131 +55,102 @@ export default {
     Fullscreen,
     User
   },
-  data () {
+  data() {
     return {
       collapsed: false,
       minLogo,
       maxLogo,
       isFullscreen: false
-    }
+    };
   },
   computed: {
-    tagNavList () {
-      return this.$store.state.app.tagNavList
+    tagNavList() {
+      return this.$store.state.app.tagNavList;
     },
-    tagRouter () {
-      return this.$store.state.app.tagRouter
+    tagRouter() {
+      return this.$store.state.app.tagRouter;
     },
-    userAvator () {
-      return this.$store.state.user.avatorImgPath
+    userAvator() {
+      return this.$store.state.user.avatorImgPath;
     },
-    cacheList () {
+    cacheList() {
       return this.tagNavList.length
         ? this.tagNavList
-          .filter(item => !(item.meta && item.meta.notCache))
-          .map(item => item.name)
-        : []
+            .filter(item => !(item.meta && item.meta.notCache))
+            .map(item => item.name)
+        : [];
     },
-    menuList () {
-      return this.$store.getters.menuList
+    menuList() {
+      return this.$store.getters.menuList;
     },
-    local () {
-      return this.$store.state.app.local
+    local() {
+      return this.$store.state.app.local;
     }
   },
   methods: {
-    ...mapMutations(['setBreadCrumb', 'setTagNavList', 'addTag', 'setLocal']),
-    ...mapActions(['handleLogin']),
-    turnToPage (route) {
-      let { name, params, query } = {}
-      if (typeof route === 'string') name = route
+    ...mapMutations(["setBreadCrumb", "setTagNavList", "addTag", "setLocal"]),
+    ...mapActions(["handleLogin"]),
+    turnToPage(route) {
+      let { name, params, query } = {};
+      if (typeof route === "string") name = route;
       else {
-        name = route.name
-        params = route.params
-        query = route.query
+        name = route.name;
+        params = route.params;
+        query = route.query;
       }
-      if (name.indexOf('isTurnByHref_') > -1) {
-        window.open(name.split('_')[1])
-        return
+      if (name.indexOf("isTurnByHref_") > -1) {
+        window.open(name.split("_")[1]);
+        return;
       }
       this.$router.push({
         name,
         params,
         query
-      })
+      });
     },
-    handleCollapsedChange (state) {
-      this.collapsed = state
+    handleCollapsedChange(state) {
+      this.collapsed = state;
     },
-    handleCloseTag (res, type, route) {
-      let openName = ''
-      if (type === 'all') {
-        this.turnToPage('home')
-        openName = 'home'
+    handleCloseTag(res, type, route) {
+      let openName = "";
+      if (type === "all") {
+        this.turnToPage("home");
+        openName = "home";
       } else if (routeEqual(this.$route, route)) {
-        if (type === 'others') {
-          openName = route.name
+        if (type === "others") {
+          openName = route.name;
         } else {
-          const nextRoute = getNextRoute(this.tagNavList, route)
-          this.$router.push(nextRoute)
-          openName = nextRoute.name
+          const nextRoute = getNextRoute(this.tagNavList, route);
+          this.$router.push(nextRoute);
+          openName = nextRoute.name;
         }
       }
-      this.setTagNavList(res)
-      this.$refs.sideMenu.updateOpenName(openName)
+      this.setTagNavList(res);
+      this.$refs.sideMenu.updateOpenName(openName);
     },
-    handleClick (item) {
-      this.turnToPage(item)
+    handleClick(item) {
+      this.turnToPage(item);
     }
   },
   watch: {
-    $route (newRoute) {
-      this.setBreadCrumb(newRoute.matched)
-      this.setTagNavList(getNewTagList(this.tagNavList, newRoute))
+    $route(newRoute) {
+      this.setBreadCrumb(newRoute.matched);
+      this.setTagNavList(getNewTagList(this.tagNavList, newRoute));
     }
   },
-  mounted () {
+  mounted() {
     /**
      * @description 初始化设置面包屑导航和标签导航
      */
-    this.setTagNavList()
+    this.setTagNavList();
     this.addTag({
       route: this.$store.state.app.homeRoute
-    })
-    this.setBreadCrumb(this.$route.matched)
+    });
+    this.setBreadCrumb(this.$route.matched);
     // 设置初始语言
-    this.setLocal(this.$i18n.locale)
-    // 文档提示
-    this.$Notice.info({
-      title: '想快速上手，去看文档吧',
-      duration: 0,
-      render: h => {
-        return h(
-          'p',
-          {
-            style: {
-              fontSize: '13px'
-            }
-          },
-          [
-            '点击',
-            h(
-              'a',
-              {
-                attrs: {
-                  href: 'https://lison16.github.io/iview-admin-doc/#/',
-                  target: '_blank'
-                }
-              },
-              'iview-admin2.0文档'
-            ),
-            '快速查看'
-          ]
-        )
-      }
-    })
+    this.setLocal(this.$i18n.locale);
   }
-}
+};
 </script>
 <style scoped>
 .logo-con {
